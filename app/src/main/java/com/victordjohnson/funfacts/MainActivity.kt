57a8699cff86fdc2bf2f21f9cdb8007ff40e23cf
Vic <victordjohnson@gmail.com>
2019-01-20 15:11:10 -0600

    added a new textview as well as a new button to the main activity

--- app/src/main/java/com/victordjohnson/funfacts/MainActivity.kt
+++ app/src/main/java/com/victordjohnson/funfacts/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.victordjohnson.funfacts
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+
+      val rand = Random()
+
+      var btn = findViewById<Button>(R.id.hello)
+      btn.setOnClickListener {toast(rand.nextInt().toString())}
     }
+  private fun Context.toast(message: CharSequence)=
+      Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
